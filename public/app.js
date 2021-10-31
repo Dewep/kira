aab7e0b24126378458c177b28de9406c4aea6d73
Maigret Aurélien <github@dewep.net>
2021-10-31 20:22:14 +0100

    Front auth+join+invitation

--- public/app.js
+++ public/app.js
@@ -1,30 +1,14 @@
 window.AppDefinition = {
   data () {
     return {
-      houses: [
-        {
-          name: 'Kira\'s house',
-          isAdmin: true,
-          cameras: [
-            {
-              slug: '4180',
-              type: 'esp32-cam-tasmota',
-              name: 'Salon',
-              options: {}
-            },
-            {
-              slug: '4181',
-              type: 'esp32-cam-tasmota',
-              name: 'Chambre',
-              options: {}
-            }
-          ],
-          logs: [
-            { date: '30 oct. 8h10, 30 oct. 18h10, 30 oct. 20h10', user: 'Malie' },
-            { date: '30 oct. 10h10, 30 oct. 21h10, 30 oct. 22h10', user: 'Edou' }
-          ]
-        }
-      ]
+      code: '',
+      invitation: {
+        opened: false,
+        name: '',
+        expiration: 10,
+        code: ''
+      },
+      houses: []
     }
   },
 
@@ -32,8 +16,65 @@ window.AppDefinition = {
   },
 
   mounted () {
+    setTimeout(() => {
+      document.body.classList.remove('initializing')
+    }, 250)
+
+    this.load()
   },
 
   methods: {
+    async load () {
+      this.houses = []
+      try {
+        const response = await this.auth()
+        window.localStorage.authorization = response.authorization
+        this.houses = response.houses
+      } catch (err) {
+        console.error(err)
+      }
+    },
+
+    async auth () {
+      const response = await this.request('/auth/', {
+        code: this.code || null
+      })
+      this.code = ''
+      return response
+    },
+
+    async invite (houseSlug) {
+      const response = await this.request('/invitation/', {
+        houseSlug,
+        name: this.invitation.name,
+        expirationMinute: this.invitation.expiration
+      })
+      this.invitation.name = ''
+      this.invitation.code = response.code
+      this.invitation.opened = false
+    },
+
+    async request (path, body, json = true) {
+      const options = {
+        method: body ? 'POST' : 'GET',
+        cache: 'no-cache',
+        headers: {
+          'Authorization': window.localStorage.authorization || ''
+        }
+      }
+
+      if (body) {
+        options.headers['Content-Type'] = 'application/json'
+        options.method = 'POST'
+        options.body = JSON.stringify(body)
+      }
+
+      const response = await fetch(path, options)
+
+      if (json) {
+        return response.json()
+      }
+      return response
+    }
   }
 }
