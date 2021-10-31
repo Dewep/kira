0d41dce106e7ce370ce93c66187364ea552bc24d
Maigret Aurélien <github@dewep.net>
2021-10-31 20:21:34 +0100

    Add invitation, improve auth

--- server/config.example.js
+++ server/config.example.js
@@ -2,5 +2,17 @@ module.exports = {
   apiKey: {
     iv: '6ee3e9d87c82417ee172e66d26ef69626ad55d6c1c', // node -p "require('crypto').randomBytes(21).toString('hex')"
     key: '3fe085559891796d8742d60443f7ceb5aed9061a89' // node -p "require('crypto').randomBytes(21).toString('hex')"
+  },
+  houses: {
+    kira: {
+      name: 'Kira',
+      cameras: {
+        salon: {
+          name: 'Salon',
+          type: 'esp32-cam-tasmota',
+          snapshot: 'http://127.0.0.1:4181/snapshot.jpg'
+        }
+      }
+    }
   }
 }
--- server/index.js
+++ server/index.js
@@ -2,24 +2,32 @@ const webServer = require('./web-server')
 const path = require('path')
 const fs = require('fs')
 const apiKey = require('./apikey')
+const config = require('./config')
+
+const availableHouseSlugs = Object.keys(config.houses)
 
 webServer.middleware(async function (transaction) {
-  transaction.houses = []
-  transaction.admins = []
+  transaction.houses = {}
 
   transaction.apiKeys = (transaction.request.headers['authorization'] || '')
     .split('.')
     .filter(secret => {
       try {
+        if (!secret) {
+          return false
+        }
         const { data } = apiKey.decrypt(secret)
         if (!data.h) {
           return false
         }
-        if (!transaction.houses.includes(data.h)) {
-          transaction.houses.push(data.h)
+        if (!availableHouseSlugs.includes(data.h)) {
+          return false
         }
-        if (data.a && !transaction.houses.includes(data.h)) {
-          transaction.admins.push(data.h)
+        if (!transaction.houses[data.h]) {
+          transaction.houses[data.h] = {
+            name: data.n,
+            isAdmin: data.a === 1
+          }
         }
         return true
       } catch (err) {
@@ -28,25 +36,107 @@ webServer.middleware(async function (transaction) {
     })
 })
 
+function expirationMinuteToTimestamp (minutes) {
+  return new Date().getTime() + minutes * 60 * 1000
+}
+
+const invitationCodes = {}
+function generateInvitationCode (options) {
+  const code = Math.random().toString('26').substr(2, 4).toUpperCase()
+  invitationCodes[code] = options
+  console.log('New invitation code:', code, ' ; options =', options)
+  return code
+}
+
 webServer.post('/auth/', async transaction => {
-  if (transaction.body.code === 'kira') {
-    const secret = apiKey.encrypt({ h: 'kira', a: 1 }, new Date().getTime() + 30 * 1000)
-    transaction.apiKeys.push(secret)
-    transaction.houses.push('kira')
-    transaction.admins.push('kira')
+  const code = transaction.body.code
+
+  if (code) {
+    if (code === 'logout') {
+      transaction.houses = {}
+      transaction.apiKeys = []
+    }
+
+    for (const houseSlug of availableHouseSlugs) {
+      const prefixSize = houseSlug.length + 1
+      if (code.startsWith(houseSlug + ':') && code.length > prefixSize + 2 && code.length < prefixSize + 30) {
+        generateInvitationCode({
+          houseSlug,
+          name: code.substr(prefixSize),
+          isAdmin: true,
+          expiration: expirationMinuteToTimestamp(60 * 24 * 365)
+        })
+      }
+    }
+
+    if (Object.keys(invitationCodes).includes(code.toUpperCase())) {
+      const options = invitationCodes[code.toUpperCase()]
+      delete invitationCodes[code.toUpperCase()]
+
+      const content = { h: options.houseSlug, n: options.name }
+      if (options.isAdmin) {
+        content.a = 1
+      }
+
+      const secret = apiKey.encrypt(content, options.expiration)
+      transaction.apiKeys.push(secret)
+
+      transaction.houses[options.houseSlug] = {
+        name: options.name,
+        isAdmin: options.isAdmin
+      }
+    }
   }
 
   const result = {
     authorization: transaction.apiKeys.join('.'),
-    houses: transaction.houses
+
+    houses: Object.keys(transaction.houses).map(houseSlug => {
+      const house = config.houses[houseSlug]
+      const houseUser = transaction.houses[houseSlug]
+
+      return {
+        slug: houseSlug,
+        name: house.name,
+        user: houseUser.name,
+        expiration: houseUser.expiration,
+        isAdmin: houseUser.isAdmin,
+        cameras: Object.keys(house.cameras).map(cameraSlug => {
+          const camera = house.cameras[cameraSlug]
+          return {
+            slug: cameraSlug,
+            name: camera.name,
+            type: camera.type
+          }
+        }),
+        logs: []
+      }
+    })
   }
 
   return transaction.json(result)
 })
 
+webServer.post('/invitation/', async transaction => {
+  const { houseSlug, name, expirationMinute } = transaction.body
+
+  if (!availableHouseSlugs.includes(houseSlug) || !transaction.houses[houseSlug] || !transaction.houses[houseSlug].isAdmin) {
+    return transaction.json({ error: 'You are not admin of this house' }, 403)
+  }
+
+  const code = generateInvitationCode({
+    houseSlug,
+    name,
+    isAdmin: false,
+    expiration: expirationMinuteToTimestamp(expirationMinute)
+  })
+
+  return transaction.json({ code })
+})
+
 const staticFile = (filename, contentType) => {
-  const content = fs.readFileSync(path.join(__dirname, '..', 'public', filename))
   return transaction => {
+    const content = fs.readFileSync(path.join(__dirname, '..', 'public', filename))
     transaction.response.writeHead(200, { 'Content-Type': contentType })
     transaction.response.end(content, 'utf-8')
     return true
